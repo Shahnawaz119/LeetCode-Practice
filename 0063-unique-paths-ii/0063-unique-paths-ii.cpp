6ae481ae171bb52f5149fe607227d8d6f2ddc191
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-07-17 12:33:58 +0530

    Time: 0 ms (100%), Space: 11.2 MB (96.58%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    int t[101][101];
     int totalWay(vector<vector<int>> &arr ,int i,int j,int n,int m){
         if(i>=n || j>=m || arr[i][j]==1){
             return 0;
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
         }
         if(i==n-1 && j==m-1){
             return 1;
         }
         int way1=totalWay(arr,i,j+1,n,m);
         int way2=totalWay(arr,i+1,j,n,m);
-        return way1+way2;
+        t[i][j]=way1+way2;
+        return t[i][j];
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
+        memset(t,-1,sizeof(t));
         if(obstacleGrid[0][0]==1 || obstacleGrid[n-1][m-1]){
             return 0;
         }
