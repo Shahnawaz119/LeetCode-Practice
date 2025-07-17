c6cf8794087cbfc10c604c991067fab9a8dd6e81
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-07-17 09:26:20 +0530

    Time: 8 ms (63.43%), Space: 18.3 MB (37.67%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+        }
+        if(abs(target) > sum || (target+sum)%2!=0){
+            return 0;
+        }
+        target=(target+sum)/2;
+        vector<vector<unsigned long long>> arr(n+1,vector<unsigned long long> (target+1,0));
+        arr[0][0]=1;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=target; j++){
+                if(nums[i-1]>j){
+                    arr[i][j]=arr[i-1][j];
+                }else{
+                    arr[i][j]=arr[i-1][j]+arr[i-1][j-nums[i-1]];
+                }
+            }
+        }
+        return arr[n][target];
+    }
+};
