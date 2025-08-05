9e9f2c470e28c9b01639c57551095ccd156225df
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-08-05 16:31:35 +0530

    Time: 3 ms (66.48%), Space: 8.1 MB (17.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1){
+        if(n==1 || n==0){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+       vector<int> arr(n+1);
+       arr[0]=0;
+       arr[1]=1;
+       for(int i=2; i<=n; i++){
+        arr[i]=arr[i-1]+arr[i-2];
+       } 
+       return arr[n];
     }
 };
