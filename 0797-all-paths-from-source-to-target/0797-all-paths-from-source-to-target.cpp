bfd7d8469ef8eadda7c478728401b6a1217b1cad
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-08-18 20:43:38 +0530

    Time: 7 ms (45.53%), Space: 13.7 MB (89.18%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void helper(vector<vector<int>> &graph,vector<vector<int>>&ans,vector<int> &temp,int src){
+    void helper(vector<vector<int>> &graph,int src,vector<int> &temp,vector<vector<int>>&ans){
         temp.push_back(src);
         if(src==graph.size()-1){
             ans.push_back(temp);
         }else{
             for(int v=0; v<graph[src].size(); v++){
-                helper(graph,ans,temp,graph[src][v]);
+                helper(graph,graph[src][v],temp,ans);
             }
         }
         temp.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
         vector<int> temp;
-        helper(graph,ans,temp,0);
+        vector<vector<int>> ans;
+        helper(graph,0,temp,ans);
         return ans;
     }
 };
