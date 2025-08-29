ac720a3a51797248a38e2dc4ac03af25351a558c
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-08-29 10:23:56 +0530

    Time: 36 ms (77.58%), Space: 57.1 MB (60.47%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -4,12 +4,12 @@ public:
         if(n==1){
             return {0};
         }
-        vector<int> indegree(n,0);
         vector<vector<int>> adj(n);
         for(int i=0; i<edges.size(); i++){
-            adj[edges[i][1]].push_back(edges[i][0]);
             adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
+        vector<int> indegree(n);
         for(int u=0; u<adj.size(); u++){
             for(int v=0; v<adj[u].size(); v++){
                 indegree[adj[u][v]]++;
@@ -21,12 +21,11 @@ public:
                 q.push(i);
             }
         }
-       
         vector<int> res;
         while(!q.empty()){
-            int n=q.size();
+            int n1=q.size();
             res.clear();
-            for(int i=0; i<n; i++){
+            for(int i=0; i<n1; i++){
                 int u=q.front();
                 q.pop();
                 res.push_back(u);
