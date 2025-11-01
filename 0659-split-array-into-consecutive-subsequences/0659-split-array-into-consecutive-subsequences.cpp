5a959313996ad0845ba65a185f4a261b7bc4b59e
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-11-01 22:00:56 +0530

    Time: 9 ms (83.31%), Space: 62.1 MB (49.33%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> count,end;
+        for(auto x:nums){
+            count[x]++;
+        }
+        for(auto x:nums){
+            if(count[x]==0){
+                continue;
+            }
+            count[x]--;
+            if(end[x-1]>0){
+                end[x-1]--;
+                end[x]++;
+            }else if(count[x+1]>0 && count[x+2]>0){
+                count[x+1]--;
+                count[x+2]--;
+                end[x+2]++;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
