38f3f03faa407e568d804b58456788e8879e39a3
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-01-10 00:46:37 +0530

    Time: 0 ms (100%), Space: 16.6 MB (13.51%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,int> mp;
+    int maxD=0;
+    TreeNode* LCA(TreeNode* root){
+        if(root==NULL || mp[root->val]==maxD){
+            return root;
+        }
+        TreeNode* left=LCA(root->left);
+        TreeNode* right=LCA(root->right);
+        if(left!=NULL && right!=NULL){
+            return root;
+        }
+        if(left){
+            return left;
+        }
+        if(right){
+            return right;
+        }
+        return NULL;
+    }
+    void depth(TreeNode* root,int d){
+        if(!root){
+            return;
+        }
+        mp[root->val]=d;
+        maxD=max(maxD,d);
+        depth(root->left,d+1);
+        depth(root->right,d+1);
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        depth(root,0);
+        return LCA(root);
+    }
+};
