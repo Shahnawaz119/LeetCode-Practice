07a13e690914bcee2f6de6d756661d48598fc9b1
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-02-14 01:32:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    int n;
+    int maxLen=0;
+    int helper(string &s,char ch1,char ch2){
+        int maxL=INT_MIN;
+        int count1=0;
+        int count2=0;
+        unordered_map<int,int> diffMap;
+        for(int i=0; i<n; i++){
+            if(s[i]!=ch1 && s[i]!=ch2){
+                diffMap.clear();
+                count1=0;
+                count2=0;
+                continue;
+            }
+            if(s[i]==ch1){
+                count1++;
+            }
+            if(s[i]==ch2){
+                count2++;
+            }
+
+            if(count1==count2){
+                maxL=max(maxL,count1+count2);
+            }
+
+
+            int diff=count1-count2;
+            if(diffMap.count(diff)){
+                maxL=max(maxL,i-diffMap[diff]);
+            }else{
+                diffMap[diff]=i;
+            }
+        }
+        return maxL;
+    }
+    int longestBalanced(string s) {
+        n=s.length();
+        if(n==1){
+            return 1;
+        }
+        // case1
+        int count=1;
+        for(int i=1; i<n; i++){
+            if(s[i]==s[i-1]){
+                count++;
+            }else{
+                maxLen=max(maxLen,count);
+                count=1;
+            }
+        }
+        //case2
+        maxLen=max(maxLen,helper(s,'a','b'));
+        maxLen=max(maxLen,helper(s,'a','c'));
+        maxLen=max(maxLen,helper(s,'b','c'));
+
+        //case3
+        int countA=0;
+        int countB=0;
+        int countC=0;
+        unordered_map<string,int> diffMap;
+        for(int i=0; i<n; i++){
+            if(s[i]=='a'){
+                countA++;
+            }
+            if(s[i]=='b'){
+                countB++;
+            }
+            if(s[i]=='c'){
+                countC++;
+            }
+
+            if(countA==countB && countA==countC){
+                maxLen=max(maxLen,countA+countB+countC);
+            }
+
+            int diffAB=countA-countB;
+            int diffAC=countA-countC;
+            string key=to_string(diffAB)+"_"+to_string(diffAC);
+            if(diffMap.count(key)){
+                maxLen=max(maxLen,i-diffMap[key]);
+            }else{
+                diffMap[key]=i;
+            }
+        }
+        return maxLen;
+    }
+};
