1a38a852c992abcfca31663901fbf9ea73784333
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-02-22 13:01:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        if(n==1){
+            return 0;
+        }
+        int maxGap=0;
+        int countZero=0;
+        int countOne=0;
+        while(n>0){
+            int currBit=(n&1);
+            if(currBit==0){
+                countZero++;
+            }else{
+                countOne++;
+                if(countOne<2){
+                    countZero=0;
+                }else{
+                    maxGap=max(maxGap,countZero+1);
+                }
+            }
+            n=n>>1;
+        }
+        return maxGap;
+    }
+};
