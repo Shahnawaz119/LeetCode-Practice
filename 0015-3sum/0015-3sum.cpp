44e14b1c9d2d5d5b455f5d432ba02dcc693a490e
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-04-01 21:35:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void twoSum(vector<int>&nums,int target,int i,int j){
+        while(i<j){
+            int sum=nums[i]+nums[j];
+            if(sum<target){
+                i++;
+            }else if(sum>target){
+                j--;
+            }else{
+                if(i<j && nums[i]==nums[i+1]){
+                    i++;
+                }
+                if(i<j && nums[j]==nums[j-1]){
+                    j--;
+                }
+                ans.push_back({-target,nums[i],nums[j]});
+                i++;
+                j--;
+            }
+        }
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int val=nums[i];
+            int target=-val;
+            twoSum(nums,target,i+1,n-1);
+        }
+        return ans;
+    }
+};
