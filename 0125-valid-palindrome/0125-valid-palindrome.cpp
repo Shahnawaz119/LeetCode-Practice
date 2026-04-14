9f89b42faa990309e70e53d7f2564034e6399d0d
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-04-15 01:47:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str="";
-        for(int i=0; i<s.length(); i++){
-            if(isalnum(s[i])){
-                str+=tolower(s[i]);
+        int n=s.length();
+        for(int i=0; i<n; i++){
+            if(isalpha(s[i])){
+                s[i]=tolower(s[i]);
             }
         }
         int i=0;
-        int j=str.length()-1;
+        int j=n-1;
         while(i<=j){
-            if(str[i]!=str[j]){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(s[i]!=s[j]){
                 return false;
             }
             i++;
