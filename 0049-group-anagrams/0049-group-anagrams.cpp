648f9c62198f8c5928ad230f034205c36fa63dc2
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-04-26 23:03:13 +0530

    Time: 13 ms (77.72%), Space: 26 MB (54.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    string generate(string& word){
-        int arr[26]={0};
-        for(int i=0; i<word.length(); i++){
-            char ch=word[i]-'a';
-            arr[ch]++;
+    string generate(string str){
+        int freq[26]={0};
+        for(int i=0; i<str.length(); i++){
+            freq[str[i]-'a']++;
         }
-        string new_string="";
+        string newStr="";
         for(int i=0; i<26; i++){
-            int freq=arr[i];
-            if(freq>0){
-                new_string+=string(freq,i+'a');
+            int count=freq[i];
+            if(count>0){
+                newStr+=string(count,i+'a');
             }
         }
-
-        return new_string;
+        return newStr;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n=strs.size();
         unordered_map<string,vector<string>> map;
         vector<vector<string>> result;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<strs.size(); i++){
             string word=strs[i];
-            string new_word=generate(word);
-            map[new_word].push_back(word);
+            string newString=generate(word);
+            map[newString].push_back(word);
         }
-
-        for(auto &it : map){
+        for(auto &it:map){
             result.push_back(it.second);
-
         }
         return result;
     }
