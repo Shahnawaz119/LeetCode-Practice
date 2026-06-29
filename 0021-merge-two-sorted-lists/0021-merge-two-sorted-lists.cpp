93f92e98dc690db3132e2616f5f14dff80c3e10d
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-06-29 11:31:11 +0530

    Time: 0 ms (100%), Space: 19.3 MB (99.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,30 +14,31 @@ public:
         if(list1==NULL && list2==NULL){
             return NULL;
         }
-        if(list1!=NULL && list2==NULL){
-            return list1;
-        }
         if(list1==NULL && list2!=NULL){
             return list2;
         }
-        ListNode* temp=new ListNode(100);
-        ListNode* temp1=temp;
+        if(list1!=NULL && list2==NULL){
+            return list1;
+        }
+        ListNode* dummay=new ListNode(100);
+        ListNode* curr=dummay;
         while(list1!=NULL && list2!=NULL){
             if(list1->val<=list2->val){
-                temp1->next=list1;
+                curr->next=list1;
+                curr=curr->next;
                 list1=list1->next;
-                temp1=temp1->next;
             }else{
-                temp1->next=list2;
+                curr->next=list2;
+                curr=curr->next;
                 list2=list2->next;
-                temp1=temp1->next;
             }
         }
-        if(list1!=NULL){
-            temp1->next=list1;
-        }else{
-            temp1->next=list2;
+        if(list1==NULL){
+            curr->next=list2;
         }
-        return temp->next;
+        if(list2==NULL){
+            curr->next=list1;
+        }
+        return dummay->next;
     }
 };
