bc23a76d167523ac33dfce077eb2658f915069fe
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-07-21 11:41:55 +0530

    Time: 0 ms (100%), Space: 21.7 MB (71.11%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0; i<n; ){
+        for(int i=0; i<n;){
             if(nums[i]<n && nums[i]!=nums[nums[i]]){
                 swap(nums[i],nums[nums[i]]);
             }else{
