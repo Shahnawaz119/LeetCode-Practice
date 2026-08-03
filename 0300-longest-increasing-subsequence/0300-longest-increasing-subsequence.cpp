08a88057ee59c8d281e0ceec9fc4250d67d23a1c
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-08-03 23:15:37 +0530

    Time: 681 ms (5.04%), Space: 203.7 MB (33.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<int>&nums,vector<int>&nums1,vector<vector<int>>&dp){
+        if(i==nums.size() || j==nums1.size()){
+            return 0;
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(nums[i]==nums1[j]){
+            return dp[i][j]=1+solve(i+1,j+1,nums,nums1,dp);
+        }
+
+        return dp[i][j]=max(solve(i+1,j,nums,nums1,dp),solve(i,j+1,nums,nums1,dp));
+        
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         unordered_set<int> s(nums.begin(),nums.end());
         vector<int> nums1(s.begin(),s.end());
         sort(nums1.begin(),nums1.end());
         int m=nums1.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(nums[i-1]==nums1[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,nums,nums1,dp);
     }
 };
