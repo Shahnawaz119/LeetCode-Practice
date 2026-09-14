96b76d79200d3a50843c98e27aed9195c7947f54
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2026-09-15 04:15:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (15.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
-        int condidate=nums[0];
-        int count=0;
-        for(int i=0; i<n; i++){
+        int candi=nums[0];
+        int count=1;
+        for(int i=1; i<n; i++){
             if(count==0){
-                condidate=nums[i];
+                candi=nums[i];
             }
-            if(condidate==nums[i]){
+            if(candi==nums[i]){
                 count++;
             }else{
                 count--;
             }
+            
         }
-        return condidate;
+        return candi;
     }
 };
